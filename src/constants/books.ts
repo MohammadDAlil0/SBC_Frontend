2a3db0badc671b6a249c033b95da2a649936743b
Mohammad Saeed Dalil <mohmaddalil5@gmail.com>
2025-04-27 01:34:38 +0300

    edit 1

--- src/constants/books.ts
+++ src/constants/books.ts
@@ -1,20 +1,20 @@
 export const books = [
   {
-    id: '93bf0c7d-18e3-42c2-8a26-821779a6377d',
+    id: 'd01c98e7-77d6-4c60-a32e-2a0d9c1bfc8b',
     title: 'كود البناء السعودي العام',
     desc: 'هذا الكود يتضمن كل شيء عن البناء والتعديلات',
     img: '/code_1.png',
     fileURL: '1fLz3Zn2aD6O-r7YqeAMn8y4KBSQYLSvn',
   },
   {
-    id: 'e046f747-0b6a-4dcc-94c9-f5448f1ce6ca',
+    id: '6f8a079b-f52c-43df-80bb-6a7dc116689a',
     title: 'الكود السعودي للمنشآت الخرسانية',
     desc: 'هذا الكود يتضمن طريقة صناعة المنتجات الخرسانية في المملكة',
     img: '/code_2.png',
     fileURL: '1ishhDYpoZv8_N2dVYbGEyL2OYLTO_7zd',
   },
   {
-    id: '8e0286ac-8872-4902-ac8c-cb854dcf5a16',
+    id: '4d2e185e-b6ab-4b6b-af6c-f3b2c79c192f',
     title: 'الكود السعودي الكهربائي',
     desc: 'هذا الكود يتضمن كل شيء عن المعدات الكهربائية',
     img: '/code_3.png',
--- src/constants/domains.ts
+++ src/constants/domains.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://147.93.87.131:3000'
+export const API_BASE_URL = 'http://147.93.87.131:3001'
 export const Files_BASE_URL = 'http://147.93.87.131:3000/api/'
 
 export const WEBSITE_BASE_URL =
