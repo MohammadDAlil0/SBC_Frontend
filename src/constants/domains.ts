8867862d0457fc6a4cd07743f3e0131b19843550
Mohammad Saeed Dalil <mohmaddalil5@gmail.com>
2025-04-30 22:18:48 +0300

    fix the domain

--- src/constants/domains.ts
+++ src/constants/domains.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3001/'
+export const API_BASE_URL = 'http://147.93.87.131:3001/'
 export const Files_BASE_URL = 'http://localhost:3000/api/'
 
 export const WEBSITE_BASE_URL =
